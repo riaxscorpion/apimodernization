5f8fd39e379656021af18459557cb13f1f52fd4b
geka001 <71638347+geka001@users.noreply.github.com>
2020-11-24 15:23:54 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ var path = require('path');
 exports.config= {
 
     directConnect: true,
-    specs: ['features/testSuites/basicSanity/05_restServicesFromScratch.feature'],
+    specs: ['features/testSuites/basicSanity/clearData.feature'],
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     getPageTimeout: 600000,
